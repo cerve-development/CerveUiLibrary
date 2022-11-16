863dfcc0d5c4791c2e11dd5fc12cad97ee6f1a18
Revok <cerve.v55@gmail.com>
2022-11-16 12:02:13 -0500

    ExtendedTheme implemented with edge to edge placement.

--- sample/src/main/java/com/cerve/co/cerveui/MainActivity.kt
+++ sample/src/main/java/com/cerve/co/cerveui/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.core.view.WindowCompat.setDecorFitsSystemWindows
