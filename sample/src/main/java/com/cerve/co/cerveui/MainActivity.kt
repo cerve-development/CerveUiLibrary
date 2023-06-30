fb73ad2d946e3ce9fce34236fc50554282eb7e1a
Revok <cerve.v55@gmail.com>
2023-06-30 10:59:35 -0400

    gradle and compose versioning bumped.

--- sample/src/main/java/com/cerve/co/cerveui/MainActivity.kt
+++ sample/src/main/java/com/cerve/co/cerveui/MainActivity.kt
@@ -3,6 +3,7 @@ package com.cerve.co.cerveui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -11,17 +12,19 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.core.view.WindowCompat.setDecorFitsSystemWindows
 import com.cerve.co.cerveui.ui.theme.DarkColorScheme
 import com.cerve.co.cerveui.ui.theme.LightColorScheme
+import com.cerve.co.material3extension.designsystem.ExtendedSystemTheme
 import com.cerve.co.material3extension.designsystem.ExtendedTheme
+import com.cerve.co.material3extension.designsystem.ExtendedTheme.colors
 
 class MainActivity : ComponentActivity() {
 
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setDecorFitsSystemWindows(window, false)
 
         setContent {
             ExtendedTheme(
@@ -31,13 +34,15 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
 
                 Scaffold(
-                    modifier = contentModifier
+                    modifier = contentModifier,
+                    containerColor = Color.Red
                 ) { innerPadding ->
 
                     Box(
                         modifier = Modifier
                             .padding(innerPadding)
-                            .fillMaxSize(),
+                            .fillMaxSize()
+                            .background(colors.primary),
                         contentAlignment = Alignment.Center
                     ) {
                         Text(text = "Hello World")
